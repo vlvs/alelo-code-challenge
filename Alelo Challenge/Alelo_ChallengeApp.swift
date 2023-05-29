29eaf3a6962d75b6d4d86800df7ec7c522cf6720
Vander <vlvsjr@gmail.com>
2023-05-29 16:56:37 -0300

    Initial Commit

---
+++ Alelo Challenge/Alelo_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alelo_ChallengeApp.swift
+//  Alelo Challenge
+//
+//  Created by Vander on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Alelo_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
